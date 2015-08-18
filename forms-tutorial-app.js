43da20a0caf67dc8362914d909b81dc67d2e8808
Vlad Balan <vlad.balan.dev@gmail.com>
2015-08-19 00:04:27 +0300

    Step 2.2: Add tasks to mongo collection

--- forms-tutorial-app.js
+++ forms-tutorial-app.js
@@ -1,17 +1,26 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  Forms.mixin(Template.TasksForm);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.TasksForm.events({
+    'click button': function (e, tmpl) {
+      e.preventDefault();
+      var task = tmpl.form.doc('task');
+      if (task && task.length) {
+        var data = {
+          task: tmpl.form.doc('task') || ''
+          , comments: tmpl.form.doc('comments') || ''
+          , createdAt: new Date()
+        }
+        Tasks.insert(data);
+        tmpl.form.doc({
+          'task': ''
+          , 'comments': ''
+        })
+      } else {
+        console.warn('No task input detected!');
+      }
     }
   });
 }
